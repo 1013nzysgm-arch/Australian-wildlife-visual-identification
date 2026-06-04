6c51a121f480d9990061a11321a7ddf013fe7ea4
xche0424 <xche0424@student.monash.edu>
2026-06-04 20:00:45 +1000

    Add query API interface

---
+++ backend/query_api.py
@@ -0,0 +1,10 @@
+class QueryAPI:
+
+    def find_by_species(self, species_name):
+        pass
+
+    def find_by_species_count(self, species_name, min_count):
+        pass
+
+    def find_by_multiple_species(self, query_tags):
+        pass
